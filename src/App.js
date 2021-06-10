aa1211079fd3ac67f63fc5668ec93e8e941617c9
umarhammed <hammedumar007@gmail.com>
2021-06-10 22:36:47 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
+import Home from './components/Home'
 import { Counter } from './features/counter/Counter'
 import './App.css'
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
+      <Home />
     </div>
   )
 }
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Home() {
+  return <div>hello Home</div>
+}
+
+export default Home
