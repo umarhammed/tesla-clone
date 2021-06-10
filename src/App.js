b11fc2b415fe9c2e024cbfcc0a5f57f603846d42
umarhammed <hammedumar007@gmail.com>
2021-06-10 22:26:31 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import Header from './components/Header'
 import { Counter } from './features/counter/Counter'
 import './App.css'
 
 function App() {
-  return <div className='App'>hello react</div>
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  )
 }
 
 export default App
